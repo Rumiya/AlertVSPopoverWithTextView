f13f923def839e0a93cd4dd5ef60d1ad1f7babb7
Rumiya <rmurtazina@gmail.com>
2016-06-30 15:08:26 -0700

    Popover view controller added.

--- AlertVSPopoverWithTextView/ViewController.swift
+++ AlertVSPopoverWithTextView/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextViewDelegate {
+class ViewController: UIViewController, UITextViewDelegate, UIPopoverPresentationControllerDelegate {
 
 
+    @IBOutlet weak var showPopoverButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,7 +25,7 @@ class ViewController: UIViewController, UITextViewDelegate {
     @IBAction func showAlert(sender: AnyObject) {
         let alertController = UIAlertController(title: "Hello, I'm alert! \n\n\n\n\n\n\n", message: "", preferredStyle: .Alert)
 
-        let rect        = CGRectMake(20, 50, 240, 150.0)
+        let rect        = CGRectMake(15, 50, 240, 150.0)
         let textView    = UITextView(frame: rect)
 
         textView.font               = UIFont(name: "Helvetica", size: 15)
@@ -32,17 +33,15 @@ class ViewController: UIViewController, UITextViewDelegate {
         textView.backgroundColor    = UIColor.whiteColor()
         textView.layer.borderColor  = UIColor.lightGrayColor().CGColor
         textView.layer.borderWidth  = 1.0
-        textView.
         textView.text               = "Enter message here"
         textView.delegate           = self
 
         alertController.view.addSubview(textView)
 
-
         let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
         let action = UIAlertAction(title: "Ok", style: .Default, handler: { action in
 
-            let msg     = (textView.textColor == UIColor.lightGrayColor()) ? "" : textView.text
+            let msg = (textView.textColor == UIColor.lightGrayColor()) ? "" : textView.text
 
             print(msg)
 
@@ -67,7 +66,29 @@ class ViewController: UIViewController, UITextViewDelegate {
     }
 
     @IBAction func showPopover(sender: AnyObject) {
+        
+        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let pc = storyboard.instantiateViewControllerWithIdentifier("Popover") as? PopoverViewController
+        pc?.modalPresentationStyle = .Popover
+
+        let popoverViewController = pc!.popoverPresentationController
+        popoverViewController?.permittedArrowDirections = .Any
+        popoverViewController?.delegate = self
+        popoverViewController?.sourceView = self.showPopoverButton
+        popoverViewController?.sourceRect = CGRect(
+            x: self.view.bounds.origin.x + 30,
+            y: self.view.bounds.origin.y + 10,
+            width: 1,
+            height: 1)
+
+        presentViewController(pc!, animated: true, completion: nil)
+
     }
+
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .None
+    }
+
 
 }
 
